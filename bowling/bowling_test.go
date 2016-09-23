efff88208dd317ce4943c9d6fc3831e5887ac507
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2016-09-23 13:29:09 +0200

    refactored tests

--- bowling/bowling_test.go
+++ bowling/bowling_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func scoreChecker(t *testing.T, input []Frame, expectedScore int, expectedError error) {
+func ensureValidScore(t *testing.T, input []Frame, expectedScore int, expectedError error) {
 	score, err := GetScore(input)
 	if err != expectedError && !(err != nil && expectedError != nil && err.Error() == expectedError.Error()) {
 		t.Fatalf("Score error : %+v, expected %+v", err, expectedError)
@@ -18,51 +18,51 @@ func scoreChecker(t *testing.T, input []Frame, expectedScore int, expectedError 
 func TestScore(t *testing.T) {
 	input := []Frame{{9, 1}, {0, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}}
 	expected := 27
-	scoreChecker(t, input, expected, nil)
+	ensureValidScore(t, input, expected, nil)
 }
 
 func TestStrikeScore(t *testing.T) {
 	input := []Frame{{10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}}
 	expected := 270
-	scoreChecker(t, input, expected, nil)
+	ensureValidScore(t, input, expected, nil)
 
 	input = []Frame{{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {10, 0}, {10, 0}}
 	expected = 30
-	scoreChecker(t, input, expected, nil)
+	ensureValidScore(t, input, expected, nil)
 }
 
 func TestSpareScore(t *testing.T) {
 	input := []Frame{{5, 5}, {5, 3}, {5, 3}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
 	expected := 31
-	scoreChecker(t, input, expected, nil)
+	ensureValidScore(t, input, expected, nil)
 
 	input = []Frame{{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {5, 5}, {5, 5}}
 	expected = 25
-	scoreChecker(t, input, expected, nil)
+	ensureValidScore(t, input, expected, nil)
 }
 
 func TestScoreArraySize(t *testing.T) {
 	input := []Frame{{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}}
 	expectedScore := 0
-	expectedError := fmt.Errorf("len must be 10")
-	scoreChecker(t, input, expectedScore, expectedError)
+	expectedError := fmt.Errorf(ErrFrameOverflow, MaxFrame)
+	ensureValidScore(t, input, expectedScore, expectedError)
 
 	input = []Frame{{1, 1}}
 	expectedScore = 0
-	expectedError = fmt.Errorf("len must be 10")
-	scoreChecker(t, input, expectedScore, expectedError)
+	expectedError = fmt.Errorf(ErrFrameOverflow, MaxFrame)
+	ensureValidScore(t, input, expectedScore, expectedError)
 }
 
 func TestScoreValuesPositiveOrNull(t *testing.T) {
 	input := []Frame{{-1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}}
 	expectedScore := 0
-	expectedError := fmt.Errorf("value must be positive or null")
-	scoreChecker(t, input, expectedScore, expectedError)
+	expectedError := fmt.Errorf(ErrNegativeFrameScore)
+	ensureValidScore(t, input, expectedScore, expectedError)
 }
 
 func TestScoreTupleLimit(t *testing.T) {
 	input := []Frame{{10, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}}
 	expectedScore := 0
-	expectedError := fmt.Errorf("tuple score limit should be <= 10")
-	scoreChecker(t, input, expectedScore, expectedError)
+	expectedError := fmt.Errorf(ErrFrameSumInvalid, StrikeScore)
+	ensureValidScore(t, input, expectedScore, expectedError)
 }
